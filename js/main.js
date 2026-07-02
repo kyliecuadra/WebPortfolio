1c9c4a5ff64d23ae7cb4381bcaac8135d434bb74
Kylie Cuadra <45311838+kyliecuadra@users.noreply.github.com>
2026-07-02 22:32:19 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -286,7 +286,7 @@ const GitHubAPI = (() => {
     'web-based-contribution-monitoring-system',
     'nxs',
     'adms',
-    'Web-Based-Management-System',
+    'cavshop',
   ];
 
   const LANG_COLORS = {
